1f1ce10533a50a2e65679475fbe442ea975dbd09
Dominik Macháček <gldkslfmsd@gmail.com>
2018-10-25 17:06:54 +0200

    another frontend part

--- mtce/urls.py
+++ mtce/urls.py
@@ -6,4 +6,6 @@ app_name = 'mtce'
 urlpatterns = [
     path('', views.index, name='index'),
     path('comparison/<int:comparison_id>/', views.comparison_detail, name='comparison_detail'),
+    path('help', views.help, name='help'),
+    path('edit', views.edit, name='edit'),
 ]
--- mtce/views.py
+++ mtce/views.py
@@ -5,26 +5,47 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Comparison
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
 
+def get_comparisons():
+    return Comparison.objects.all() #order_by('name') \
+       # .annotate(documents=Count('document')) \
+       # .annotate(total_sentences=Count('document__sentence'))
+    # tasks_table = TaskTable(tasks)
 
 def index(request):
     """
     The initial dashboard view with a list of tasks
     """
-    comparisons = Comparison.objects.all() #order_by('name') \
-       # .annotate(documents=Count('document')) \
-       # .annotate(total_sentences=Count('document__sentence'))
-    # tasks_table = TaskTable(tasks)
+    comparisons = get_comparisons()
 
     return render(
         request,
         'mtce/index.html',
-        {'comparisons': comparisons},
+        {'comparisons': comparisons,
+         'active': 'comp'},
+    )
+
+def help(request):
+    comparisons = get_comparisons()
+
+    return render(
+        request,
+        'mtce/help.html',
+        {'comparisons': comparisons,
+         'active': 'help'},
+    )
+
+def edit(request):
+    comparisons = get_comparisons()
+    return render(
+        request,
+        'mtce/edit.html',
+        {'comparisons': comparisons,
+         'active': 'edit'},
     )
 
 def comparison_detail(request, comparison_id):
+
 
     comp = get_object_or_404(Comparison, pk=comparison_id)
 
@@ -32,5 +53,8 @@ def comparison_detail(request, comparison_id):
 
     return render(request,
                   'mtce/comparison_detail.html',
-                  {'comparison': comp}
+                  {'comparison': comp,
+                   'comparisons': get_comparisons(),
+                   'active': 'comp'}
                   )
+
